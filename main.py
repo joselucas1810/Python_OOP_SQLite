5576c7b31b69bbad3f419f2d3555cde492d7963a
José Lucas <111397299+joselucas1810@users.noreply.github.com>
2022-11-24 01:17:17 +0000

    Fiz a consulta a banco, mas a parte de inserir ao banco ta com algum problema

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-#Importar o arquivo Pessoa.py no diretório model
+#Importa nosso arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
-jose = Pessoa(1,"José Lucas")
-print (jose)
+jose = Pessoa(1, "José lucas")
+print(jose)
 
-#Mostrar apenas o nome
-print (jose.nome)
+#Quero mostrar só o nome
+print(jose.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO...")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa(0, "Denzel Washington")
+
+#Olha que simples...
+novo = pessoaDAO.save(novo)
+
+#consulta o banco de novo..
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
