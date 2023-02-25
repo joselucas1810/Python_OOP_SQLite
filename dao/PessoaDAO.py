7a8ce8530510db0e7acb176c768249d769f71292
José Lucas <111397299+joselucas1810@users.noreply.github.com>
2023-02-25 00:29:18 +0000

    Agora com a inserção funcionando

--- dao/PessoaDAO.py
+++ dao/PessoaDAO.py
@@ -1,13 +1,10 @@
 from model.Pessoa import Pessoa 
-
 class PessoaDAO:
   conexao = None
   cursor = None
-
   def __init__(self, con, cur):
     self.conexao = con
     self.cursor = cur
-
   def getAll(self, orderBy = False):
     sql = "SELECT id, nome FROM pessoa "
     if orderBy == True:
@@ -16,12 +13,10 @@ class PessoaDAO:
     try:
       self.cursor.execute(sql)
       resultado = self.cursor.fetchall()
-
       pessoas = []
       for linha in resultado:
         pessoa = Pessoa(linha[0], linha[1])
         pessoas.append(pessoa)
-
       return pessoas
     except Exception as e:
       return e
@@ -29,11 +24,14 @@ class PessoaDAO:
   #Função/método para inserir no banco.
   def save(self, pessoa):
     sql = "INSERT INTO pessoa (nome) VALUES (%s)"
+    sql = f"INSERT INTO pessoa (nome) VALUES ('{pessoa.nome}')"
 
     try:
       self.cursor.execute(sql, pessoa.nome)
+      self.cursor.execute(sql)
       self.conexao.commit()
       pessoa.id = self.cursor.lastrowid 
+      print(pessoa)
       return pessoa
     except Exception as e:
       return e
